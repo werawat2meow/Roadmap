a599c5a5abe9b153948d3d8da3470e1902cb92b8
kittiphit <phum@onephuket.co>
2026-06-12 15:28:48 +0700

    new setting

---
+++ app/recruitment/setting/language/page.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+export default function RecruitmentPage() {
+    return (
+    
+      <div className="flex h-full">
+        <div className="overflow-y-auto p-6 w-full">
+          <div className="flex items-center justify-between rounded-3xl bg-white p-6 shadow-sm">
+            <div>
+              <h1 className="text-2xl font-bold text-slate-800">
+                 Language Setting
+              </h1>
+
+              <p className="mt-2 text-slate-500">
+                หน้านี้เป็นหน้า setting สำหรับแสดง ภาษาต่างๆ ของหน้ารายละเอียดการรับสมัครงาน
+              </p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+  );
+}

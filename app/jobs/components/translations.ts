b508d6dc09e31f9100b0cb7e56728e2d2fc89c94
kittiphit <phum@onephuket.co>
2026-07-01 14:21:55 +0700

    setting

---
+++ app/jobs/components/translations.ts
@@ -0,0 +1,46 @@
+export const uiText = {
+  responsibilities: {
+    EN: "Responsibilities",
+    TH: "หน้าที่และความรับผิดชอบ",
+  },
+  qualifications: {
+    EN: "Qualifications",
+    TH: "คุณสมบัติ",
+  },
+  benefits: {
+    EN: "Benefits",
+    TH: "สวัสดิการ",
+  },
+  aboutPosition: {
+    EN: "About Position",
+    TH: "รายละเอียดตำแหน่ง",
+  },
+  apply: {
+    EN: "Apply Now",
+    TH: "สมัครงาน",
+  },
+  position: {
+    EN: "Position",
+    TH: "ตำแหน่ง",
+  },
+  jobDetails: {
+    EN: "Job Details",
+    TH: "รายละเอียดงาน",
+  },
+  workType: {
+    EN: "Work Type",
+    TH: "ประเภทงาน",
+  },
+  workLocation: {
+    EN: "Work Location",
+    TH: "สถานที่ปฏิบัติงาน",
+  },
+  salary: {
+    EN: "Salary",
+    TH: "เงินเดือน",
+  },
+  openings: {
+    EN: "Open Positions",
+    TH: "ตำแหน่งเปิดรับ",
+  },
+} as const;
